76cafcf40d90091ea4b90e241a0e9b993b0f9fda
MU-SHAFIUR RAHMAN <166214370+mushafiur@users.noreply.github.com>
2024-08-15 08:24:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// ex1
+function bulbOn(){
+	document.getElementById('img').src="images/bulbon.gif";
+}
+
+// ex2
+function bulboff(){
+	document.getElementById('img').src="images/bulboff.gif";
+}
+
+// ex3
+function changeText(){
+	document.getElementById('c_text').innerHTML="hello javascript";
+}
+
+// ex4
+function showContent(){
+	document.getElementById('s_content').innerHTML="www= world wide web";	
+}
+
+// ex5
+function hideContent(){
+	document.getElementById('h_content').style.display="none";	
+}
+// ex6
+function blockContent(){
+	document.getElementById('h_content').style.display="block";	
+}
+
+// ex7
+function textSize(){
+	document.getElementById('35px').style.fontSize="35px";
+}
+
+// ex8
+function textColor(){
+	document.getElementById('color').style.color="green";
+}
+
+// ex9
+function chargeBattery(){
+	document.getElementById('b_icon').src="images/charge.png";
+} 
+
+// ex10
+function mediumBattery(){
+	document.getElementById('b_icon').src="images/medium.png";
+}
+
+// ex11
+function highBattery(){
+	document.getElementById('b_icon').src="images/high.png";
+}
+
+// ex10
+function fullBattery(){
+	document.getElementById('b_icon').src="images/full.png";
+}
